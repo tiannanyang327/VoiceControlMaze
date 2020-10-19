5162412c1e4d17350e4bedcf63653454a06d3e3d
WanHsuanChiang <ezgtt324@gmail.com>
2020-10-19 16:47:29 +0800

    Change Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import view.SelectCharacterView;
 import view.SelectLevelView;
@@ -10,6 +11,9 @@ import view.SelectLevelView;
 import java.io.IOException;
 
 public class Main extends Application {
+	
+	public static StackPane root;
+	
     public int characterNum;
     public int mapNum;
 
@@ -19,24 +23,37 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+    	
+    	root = new StackPane();
+    	root.setPrefSize(800, 800);
+    	Scene scene = new Scene(root);
+    	
+    	
+    	
         System.out.println("main:"+this.characterNum);
         App app = new App(this.mapNum,this.characterNum);
         Pane mapView = new Pane();
         mapView.getChildren().add(app);
-        Scene MapScene = new Scene(mapView,800,800);
+        //Scene MapScene = new Scene(mapView,800,800);
 
 
         SelectCharacterView scv= new SelectCharacterView();
         SelectLevelView slv= new SelectLevelView();
 
-        Scene ChaScene = new Scene(scv,800,800);
-        Scene LevScene = new Scene(slv,800,800);
+        //Scene ChaScene = new Scene(scv,800,800);
+        //Scene LevScene = new Scene(slv,800,800);
 
 
-        scv.btnnext.setOnMouseClicked( e->primaryStage.setScene(LevScene));
-        slv.btnnext.setOnMouseClicked( e-> primaryStage.setScene(MapScene));
-        slv.previous.setOnMouseClicked(e-> primaryStage.setScene(ChaScene));
-        app.exitBtn.setOnMouseClicked(e -> primaryStage.setScene(LevScene));
+        scv.btnnext.setOnMouseClicked( e-> {
+        	root.getChildren().add(slv);
+        	});
+        //slv.btnnext.setOnMouseClicked( e-> primaryStage.setScene(MapScene));
+        slv.previous.setOnMouseClicked(e-> {
+        	root.getChildren().add(scv);
+        	});
+        app.exitBtn.setOnMouseClicked(e -> {
+        	root.getChildren().add(slv);
+        	});
 
 
         scv.char1.setOnMouseClicked( e -> {
@@ -100,8 +117,9 @@ public class Main extends Application {
             }
         });
 
+        root.getChildren().add(scv);
         primaryStage.setTitle("VoiceControlMario");
-        primaryStage.setScene(ChaScene);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
